ec24f58b464e855b80b52840c7681ec2e8a66d50
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-01-30 02:45:56 +0100

    [add] profiles

--- src/main/java/pl/karnecki/shopapp/service/CartService.java
+++ src/main/java/pl/karnecki/shopapp/service/CartService.java
@@ -1,9 +1,12 @@
 package pl.karnecki.shopapp.service;
 
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import pl.karnecki.shopapp.model.Product;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
@@ -12,20 +15,40 @@ import java.util.Random;
 
 @Slf4j
 @Service
+@Setter
+@ConfigurationProperties(prefix = "shop-values")
 public class CartService {
 
     private List<Product> productList;
+
+    private double tax;
+    private int discount;
+
+    final double TAX = (1 + tax / 100);
 
     public CartService() {
 
         Random random = new Random();
         //BigDecimal price = new BigDecimal(random.nextDouble() * 250 + 50).setScale(2, RoundingMode.DOWN);
 
-        Product product1 = new Product("Tablet", BigDecimal.valueOf(random.nextDouble() * 250 + 50).setScale(2, RoundingMode.DOWN));
-        Product product2 = new Product("Spodnie", BigDecimal.valueOf(random.nextDouble() * 250 + 50).setScale(2, RoundingMode.DOWN));
-        Product product3 = new Product("Buty", BigDecimal.valueOf(random.nextDouble() * 250 + 50).setScale(2, RoundingMode.DOWN));
-        Product product4 = new Product("Koszula", BigDecimal.valueOf(random.nextDouble() * 250 + 50).setScale(2, RoundingMode.DOWN));
-        Product product5 = new Product("Marynarka", BigDecimal.valueOf(random.nextDouble() * 250 + 50).setScale(2, RoundingMode.DOWN));
+        final int DISCOUNT = (1 - discount / 100);
+
+
+        Product product1 = new Product("Tablet", BigDecimal.valueOf(
+                (random.nextDouble() * 250 + 50) * DISCOUNT)
+                .setScale(2, RoundingMode.DOWN));
+        Product product2 = new Product("Spodnie", BigDecimal.valueOf(
+                (random.nextDouble() * 250 + 50) * DISCOUNT)
+                .setScale(2, RoundingMode.DOWN));
+        Product product3 = new Product("Buty", BigDecimal.valueOf(
+                (random.nextDouble() * 250 + 50) * DISCOUNT)
+                .setScale(2, RoundingMode.DOWN));
+        Product product4 = new Product("Koszula", BigDecimal.valueOf(
+                (random.nextDouble() * 250 + 50) * DISCOUNT)
+                .setScale(2, RoundingMode.DOWN));
+        Product product5 = new Product("Marynarka", BigDecimal.valueOf(
+                (random.nextDouble() * 250 + 50) * DISCOUNT)
+                .setScale(2, RoundingMode.DOWN));
 
         productList = new ArrayList<>();
 
@@ -39,11 +62,12 @@ public class CartService {
 
     public List<Product> getProductList() {
 
-        log.info("Zwracam liste produktow");
+        log.info("Zwracam liste produktow z uwzględnieniem rabatu w wysokosci " + discount + "%");
         return productList;
     }
 
     public BigDecimal getTotalPrice() {
+
 
         log.info("Zwracam calkowity koszt produktow w koszyku");
 
@@ -51,6 +75,9 @@ public class CartService {
         for (Product p : productList) {
             total = total.add(p.getPrice());
         }
-        return total;
+        log.info("Zawartość koszyka wynosi " + total + " , w tym " + tax + "% podatku (" +
+                total.multiply(BigDecimal.valueOf(tax / 100)).setScale(2, RoundingMode.UP) + ")");
+
+        return total.multiply(BigDecimal.valueOf(TAX)).setScale(2, RoundingMode.UP);
     }
 }
